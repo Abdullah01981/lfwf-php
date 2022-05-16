47812485a6594c4c5cfd97884cce008776ef5b22
Abdullah All Minhaz <73273173+Abdullah01981@users.noreply.github.com>
2022-05-16 16:07:25 +0600

    no

---
+++ assignment 9/in.php
@@ -0,0 +1,41 @@
+<?php 
+// echo " your action secess";
+// echo "<pre>";
+// print_r($_POST);
+
+// echo "</pre>";
+
+// if (isset($_GET['sub'])) {
+
+
+//      $user= $_GET['user']; 
+//      $pass =$_GET['pass'];
+//     $number= $_GET['number'];
+    
+    
+//     if  ( $user =="" || $pass =="" || $number =="") {
+//         echo "every thisng not ok";
+//     }
+//     else{
+//         echo " every this is ok ";
+//     }
+//     }
+
+
+
+
+
+
+
+
+// $user= $_GET['user']; 
+// $pass =$_GET['pass'];
+// $number= $_GET['number'];
+
+
+// if ( $user ==" " || $pass == " " || $number == " ") {
+//     echo '<script> alert("every thisng not ok"); </script> ';
+// }
+// else{
+//     echo " every this is ok ";
+// }
